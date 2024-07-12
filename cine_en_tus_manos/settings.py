ec1f889cf522014bc307b578169c3cef697be18a
David Valadez Gutierrez <111003902+davidvaladez09@users.noreply.github.com>
2024-07-12 14:02:29 -0600

    Update settings.py

--- cine_en_tus_manos/settings.py
+++ cine_en_tus_manos/settings.py
@@ -138,7 +138,13 @@ USE_TZ = True
 #STATIC_URL = 'static/'
 
 # Configuración de archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mainapp/static'),
 ]
