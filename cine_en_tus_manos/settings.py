3a743f9ade79efcbe482e6a3386308e66344ac87
davidvaladez09 <davidvaladezgutierrez09@gmail.com>
2024-07-18 20:16:17 -0600

    Actualizacion de settings.py para deploy de prueba en aws

--- cine_en_tus_manos/settings.py
+++ cine_en_tus_manos/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'cine_en_tus_manos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -100,7 +100,6 @@ DATABASES = {
     }
 } 
 
-'''
 import dj_database_url
 from decouple import config
 
