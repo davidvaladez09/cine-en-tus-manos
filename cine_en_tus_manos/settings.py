22b46adb4eb3786d9ae456b8c91dec37e69493e0
davidvaladez09 <davidvaladezgutierrez09@gmail.com>
2024-07-18 20:12:30 -0600

    Actualizacion de settings.py

--- cine_en_tus_manos/settings.py
+++ cine_en_tus_manos/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+&1mjrg&ay$$hn2b2vypj!g6m*dsxwr*hou9lv3dc#ts4052$8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['18.188.221.220']
 
 
 # Application definition
